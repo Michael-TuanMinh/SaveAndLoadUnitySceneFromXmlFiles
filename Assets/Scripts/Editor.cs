56133d33000318f7ff9986d8437a7e2a4c21ec2c
Michael Vu <vuminh99hp@gmail.com>
2020-03-30 01:51:58 -0400

    Final

---
+++ Assets/Scripts/Editor.cs
@@ -0,0 +1,28 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class Editor
+{
+    [MenuItem("Scenes Manager/Save Scene")]
+    static void SaveScene()
+    {
+        string path = EditorUtility.SaveFilePanel("Save Scene", Application.dataPath, "Scene", "xml");
+        if (path.Length != 0)
+        {
+            SaveAndLoadXML.SaveToXML(path);
+        }
+    }
+
+    [MenuItem("Scenes Manager/Load Scene")]
+    static void LoadScene()
+    {
+        string path = EditorUtility.OpenFilePanel("Load Scene", Application.dataPath, "xml");
+        if (path.Length != 0)
+        {
+            SaveAndLoadXML.LoadFromXML(path);
+        }
+
+    }
+
+
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+GAME3015 A2 - Tuan Minh Vu
+------------------------------------
+Load: Scenes Manager -> Load Scene
+Save: Scenes Manager -> Save Scene (Save transform, mesh, ect)
